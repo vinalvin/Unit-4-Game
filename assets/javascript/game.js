c92d90e6382c3fdf1d4f9bbd579189f04988fa90
Junming Zhang <vinalvin1010@gmail.com>
2019-07-18 17:20:07 -0700

    js files random num

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    var wins = 0;
+    var losses = 0;
+    var random = [];
+
+    function startGame() {
+        var randomNum = Math.floor(Math.random() * 102) + 19;
+        random.push(randomNum);
+    }
+})
