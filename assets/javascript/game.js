b86d2f89106fdf1d6568089ff88e9fbf26315335
Junming Zhang <vinalvin1010@gmail.com>
2019-07-19 17:51:27 -0700

    Text out on pages

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,9 +2,22 @@ $(document).ready(function() {
     var wins = 0;
     var losses = 0;
     var random = [];
+    var crystals = [];
+
+    //The player will be shown a random number at the start of the game.
 
     function startGame() {
         var randomNum = Math.floor(Math.random() * 102) + 19;
         random.push(randomNum);
+    }
+
+    var score;
+    var guesses;
+
+    function pushText() {
+        $("#wins").text(wins);
+        $("#losses").text(losses);
+        $("#score").text(score);
+        $("#guesses").text(guesses);
     }
 })
