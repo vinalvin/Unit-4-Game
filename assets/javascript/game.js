ba139245d9c9a0690b1d3a477946bd41168ff5e7
Junming Zhang <vinalvin1010@gmail.com>
2019-07-23 22:37:11 -0700

    Draft

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,27 @@
-$(document).ready(function() {
-    var wins = 0;
-    var losses = 0;
-    var random = [];
-    var crystals = [];
+var wins = 0;
+var losses = 0;
+var random = [];
+var crystals = [];
 
-    //The player will be shown a random number at the start of the game.
+//The player will be shown a random number at the start of the game.
 
-    function startGame() {
-        var randomNum = Math.floor(Math.random() * 102) + 19;
-        random.push(randomNum);
-    }
+function startGame() {
+    var randomNum = Math.floor(Math.random() * 102) + 19;
+    random.push(randomNum);
+}
 
-    var score;
-    var guesses;
+var score;
+var guesses;
 
-    function pushText() {
-        $("#wins").text(wins);
-        $("#losses").text(losses);
-        $("#score").text(score);
-        $("#guesses").text(guesses);
-    }
-})
+function pushText() {
+    $("#wins").text(wins);
+    $("#losses").text(losses);
+    $("#score").text(score);
+    $("#guesses").text(guesses);
+}
+
+$(".crystals img").click.function(
+    var goingIMG = $(this).attr("data-type");
+
+    total += crystals[goingIMG]
+)
